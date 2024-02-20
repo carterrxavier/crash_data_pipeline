c701c6d6898f9389cdde0ca43adadc2320ed59c3
Xavier Carter <80718296+carterrxavier@users.noreply.github.com>
2024-02-20 06:06:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 from src.aquire import scrape_data
 
@@ -7,7 +7,13 @@ app = Flask(__name__)
 #trigger daily, will take a request that will allow for a start date, an end date, and a city (only if the city is supported)
 @app.route("/start_scrape", methods=["POST"])
 def scrape():
-    data = json.loads(request.data.decode('utf-8'))
-    print("I SEE YOU")
-    print(data)
-    return 'success',200
+    data = request.get_json()  # Extract JSON data from the request
+    if data:
+        start_time = data.get('start_time')
+        end_time = data.get('end_time')
+        city = data.get('city')
+        print("Received data: ", data)
+        # Now you can use start_time, end_time, and city in your application logic
+        return jsonify({"message": "Data received successfully."}), 200
+    else:
+        return jsonify({"error": "No JSON data received."}), 400
