dc2e9d5adc9783678d435e90cfb15ac70223c57d
Xavier Carter <80718296+carterrxavier@users.noreply.github.com>
2024-02-19 22:33:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #trigger daily, will take a request that will allow for a start date, an end date, and a city (only if the city is supported)
 @app.route("/start_scrape", methods=["POST"])
 def scrape():
-    data = json(request.data.decode('utf-8'))
+    data = json.loads(request.data.decode('utf-8'))
     print("I SEE YOU")
     print(data)
     return 'success',200
