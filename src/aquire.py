c9398306d7ddd06226aa92666a9fca1df636fef6
carterrxavier <carter.r.xavier@gmail.com>
2024-02-20 06:49:25 -0600

    add support for major cities in texas

--- src/aquire.py
+++ src/aquire.py
@@ -16,9 +16,43 @@ os.environ['webdriver.chrome.driver'] = chromedriver
 pattern = r"https://app\.myaccident\.org/accident/\d+$"
 
 
-def scrape_data(start_date, end_date, city):  
+def check_city_support(city):
+        city_coordinates = {
+                'houston': [29.7604, -95.3698],
+                'san antonio': [29.4241, -98.4936],
+                'dallas': [32.7767, -96.7970],
+                'austin': [30.2672, -97.7431],
+                'fort worth': [32.7555, -97.3308],
+                'el paso': [31.7619, -106.4850],
+                'arlington': [32.7357, -97.1081],
+                'corpus christi': [27.8006, -97.3964],
+                'plano': [33.0198, -96.6989],
+                'laredo': [27.5306, -99.4803],
+                'lubbock': [33.5779, -101.8552],
+                'garland': [32.9126, -96.6389],
+                'irving': [32.8140, -96.9489],
+                'amarillo': [35.2210, -101.8313],
+                'grand prairie': [32.7459, -96.9978],
+                'brownsville': [25.9018, -97.4975],
+                'mckinney': [33.1972, -96.6397],
+                'frisco': [33.1507, -96.8236],
+                'pasadena': [29.6911, -95.2091],
+                'mesquite': [32.7668, -96.5992],
+                'killeen': [31.1171, -97.7278],
+                'mcallen': [26.2034, -98.2300],
+                'carrollton': [32.9756, -96.8897],
+                'beaumont': [30.0802, -94.1266],
+                'round rock': [30.5083, -97.6789],
+                'waco': [31.5493, -97.1467],
+                'denton': [33.2148, -97.1331],
+                'midland': [31.9973, -102.0779],
+                'wichita falls': [33.9137, -98.4934]
+        }
+        return city_coordinates.get(city.lower())
         
-        url = 'https://app.myaccident.org/results?startDate=2024-02-01&endDate=2024-02-18&lat=29.432050423986603&lng=-98.48402088539248&radius=20000'
+def scrape_data(start_date, end_date, city):  
+
+        url = 'https://app.myaccident.org/results?startDate=2024-02-01&endDate=2024-02-18&lat=32.7767&lng=-96.7970&radius=20000'
 
         driver = webdriver.Chrome(options=options)     
         driver.get(url)
