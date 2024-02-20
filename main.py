99d3dcb2c745c1e9499dd3351f367a7014e6bc23
Xavier Carter <80718296+carterrxavier@users.noreply.github.com>
2024-02-19 19:49:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from src.aquire import scrape_data
 
 app = Flask(__name__)
 
-#trigger daily, will take a request that will allow for a start date, an end date and a city (only if the city is supported)
+#trigger daily, will take a request that will allow for a start date, an end date, and a city (only if the city is supported)
 @app.route("/start_scrape", methods=["POST"])
 def scrape():
     data = json(request.data.decode('utf-8'))
     print(data)
+    return 'success',200
