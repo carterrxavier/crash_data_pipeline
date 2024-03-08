cac485ced2ef34394e340140ae909d4466ac7601
carterrxavier <carter.r.xavier@gmail.com>
2024-03-07 22:02:57 -0600

    store based on city and state

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask, request, jsonify
 from google.cloud import storage
 from datetime import datetime, timedelta, timezone
 import os
-from src.store_to_gcs import stitch_data, store_to_bigquery
+from src.store_to_gcs import stitch_data, store_to_bigquery, get_unique_city_states
 from src.last_run import get_last_message_timestamp
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-
 
 project_id = os.environ.get('PROJECT_ID')
 bucket_name = os.environ.get('BUCKET_NAME')
@@ -23,21 +22,34 @@ def store_to_bq():
       client = storage.Client(project=project_id)
       bucket = client.get_bucket(bucket_name)
 
-      accident_data = []
-      vehicle_data = []
-      occupant_data = []
-      time_threshold =  (datetime.today() - timedelta(hours=48)).replace(tzinfo=timezone.utc)
+      time_threshold =  (datetime.today() - timedelta(hours=24)).replace(tzinfo=timezone.utc)
       blobs = bucket.list_blobs()
       recent_files = [blob.name for blob in blobs if blob.time_created >= time_threshold]
+      city_state = []
 
+      #first get all unique city state combos from file name
       for file in recent_files:
-            stitch_data('accidents', file, accident_data)
-            stitch_data('vehicles', file, vehicle_data)
-            stitch_data('occupants', file, occupant_data)
+            state = file.split('/')[0]
+            city = file.split('/')[1]
+            city_state.append([city,state])
 
-      store_to_bigquery(accident_data,'accidents')
-      store_to_bigquery(vehicle_data,'vehicles')
-      store_to_bigquery(occupant_data,'occupants')
-      
+      unique_city_state = get_unique_city_states(city_state)
+
+
+
+      for city_state in unique_city_state:
+            blobs = bucket.list_blobs()
+            current_files = [blob.name for blob in blobs if city_state[0] in blob.name and city_state[1] in blob.name]
+            file_stitch_accidents = []
+            file_stitch_occupants = []
+            file_stitch_vehicles = []
+            for file in current_files:
+                  stitch_data('accidents', file, file_stitch_accidents)
+                  stitch_data('occupants', file, file_stitch_occupants)
+                  stitch_data('vehicles', file, file_stitch_vehicles)
+            store_to_bigquery(file_stitch_accidents,f'{city_state[0].replace(' ','_')}_{city_state[1].replace(' ','_')}_accidents')
+            store_to_bigquery(file_stitch_occupants,f'{city_state[0].replace(' ','_')}_{city_state[1].replace(' ','_')}_occupants')
+            store_to_bigquery(file_stitch_vehicles,f'{city_state[0].replace(' ','_')}_{city_state[1].replace(' ','_')}_vehicles')
+
       return "success",200
 store_to_bq()
--- src/store_to_gcs.py
+++ src/store_to_gcs.py
@@ -4,6 +4,7 @@ from google.cloud import bigquery
 import json
 from dotenv import load_dotenv
 load_dotenv()
+from functools import reduce
 
 
 project_id = os.environ.get('PROJECT_ID')
@@ -26,7 +27,12 @@ def store_to_cloud(file_type,file, state, city, start_date, end_date):
 
     print(f"JSON data has been uploaded to {blob.public_url}")
 
-def stitch_data(file_type, file_name, file_stitch):  
+
+
+def get_unique_city_states(list):
+      return reduce(lambda re, x: re+[x] if x not in re else re,list,[])
+
+def stitch_data(file_type, file_name,file_stitch):  
     if file_type in file_name:
         blob = bucket.blob(file_name)
         json_string = blob.download_as_string()
@@ -50,34 +56,36 @@ def store_to_bigquery(json_data,table):
         dataset = client.create_dataset(dataset)
         client.get_dataset(dataset_ref)
 
-
-    # Infer schema from the JSON data
-    first_json_object = json_data[0]
-
-    schema = []
-    for key in first_json_object.keys():
-        # Add each key-value pair as a field in the schema
-        schema.append(bigquery.SchemaField(key, "STRING"))
-    
-
-    # Create or get the table reference
     try:
-        table_ref = dataset_ref.table(table_id)
-        table = bigquery.Table(table_ref, schema=schema)
-        client.get_table(table_ref)
-    except:
-        # Create the table with the inferred schema
-        table = client.create_table(table)
-        table_ref = dataset_ref.table(table_id)
-        table = bigquery.Table(table_ref, schema=schema)
-        client.get_table(table_ref)
+        # Infer schema from the JSON data
+        first_json_object = json_data[0]
+
+        schema = []
+        for key in first_json_object.keys():
+            # Add each key-value pair as a field in the schema
+            schema.append(bigquery.SchemaField(key, "STRING"))
         
-    # Load the JSON data into BigQuery
-    job_config = bigquery.LoadJobConfig()
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job = client.load_table_from_json(json_data, table_ref, job_config=job_config)
 
-    # Wait for the job to complete
-    job.result()
+        # Create or get the table reference
+        try:
+            table_ref = dataset_ref.table(table_id)
+            table = bigquery.Table(table_ref, schema=schema)
+            client.get_table(table_ref)
+        except:
+            # Create the table with the inferred schema
+            table = client.create_table(table)
+            table_ref = dataset_ref.table(table_id)
+            table = bigquery.Table(table_ref, schema=schema)
+            client.get_table(table_ref)
+            
+        # Load the JSON data into BigQuery
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job = client.load_table_from_json(json_data, table_ref, job_config=job_config)
 
-    print(f"Loaded {job.output_rows} row(s) into {table_id}")
+        # Wait for the job to complete
+        job.result()
+
+        print(f"Loaded {job.output_rows} row(s) into {table_id}")
+    except:
+        print(f'Error uploading files related to {ta}')
